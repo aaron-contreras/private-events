79bf628260138ee73d2ccf54a537a4da09f8ea1a
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-10 18:33:31 -0500

    Add signup feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,27 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new    
   end
   
   def create
+    @user = User.new(user_params)    
     
+    if @user.save
+      flash[:success] = "You've signed up succesfully"
+      redirect_to @user
+    else
+      flash[:alert] = 'Unsuccesful signup'
+      render :new
+    end
   end
   
   def show
-    
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username)
   end
 end
