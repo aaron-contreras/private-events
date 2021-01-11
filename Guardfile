5576b885763ef4d805d234922bb8399f768586a0
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-11 08:55:13 -0500

    Add events

--- Guardfile
+++ Guardfile
@@ -55,6 +55,8 @@ guard 'livereload' do
   # file needing a full reload of the page anyway
   watch(%r{app/views/.+\.(#{rails_view_exts * '|'})$})
   watch(%r{app/helpers/.+\.rb})
+  watch(%r{app/controllers/.+\.rb})
+  watch(%r{app/models/.+\.rb})
   watch(%r{config/locales/.+\.yml})
 end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  has_many   :attendances
+  has_many   :attendees, through: :attendances, class_name: 'User',
+                       foreign_key: 'user_id', source: :user
+  scope :past, -> { where('date < ?', Time.zone.now) }
+  scope :upcoming, -> { where('date > ?', Time.zone.now) }
 end
+
