4e0f1001951eb77556e9cd36fd505c91632759c5
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-11 17:17:38 -0500

    Refactor log in form to partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user
     else
+      flash[:alert] = 'Invalid username.'
       render :new 
     end
   end
