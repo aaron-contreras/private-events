2d8aea853a0ff47aa4351b2322fd12fa37927278
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-11 14:18:53 -0500

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :attendances
   has_many :attended_events, through: :attendances, class_name: 'Event',
                              foreign_key: 'event_id', source: :event
+
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
 end
