b16f81a8306a286e5a471cfb2631c72b6e8d0ec6
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-11 09:33:13 -0500

    Add nav links and logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'events#index'
+
   resources :users,    only: [:new, :create, :show]
-  resources :sessions, only: [:new, :create]
   resources :events,   only: [:new, :create, :show, :index]
+  resources :sessions, only: [:new, :create]
+
+  get '/logout', to: 'sessions#destroy'
 end
