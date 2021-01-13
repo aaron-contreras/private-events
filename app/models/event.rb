9be4c7147444b61ebf2a03171bed418346983fab
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-12 22:01:57 -0500

    Add helper method to get date in english

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,13 @@ class Event < ApplicationRecord
   has_many   :attendees, through: :attendances, class_name: 'User',
                        foreign_key: 'user_id', source: :user
   has_many :invitations
+
   validates :description, :date, :location, presence: true
+
   scope :past, -> { where('date < ?', Time.zone.now) }
   scope :upcoming, -> { where('date > ?', Time.zone.now) }
-end
 
+  def english_date
+    date.strftime('%A, %B %d, %Y')
+  end
+end
